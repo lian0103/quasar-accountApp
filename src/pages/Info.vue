ec9db66f85f80b4002a9cc536bfe0d53e8a52cac
Jason <gt07891@greattree.com.tw>
2022-01-13 17:39:50 +0800

    fix info

--- src/pages/Info.vue
+++ src/pages/Info.vue
@@ -3,14 +3,14 @@ import { useUserStore } from "../stores/userInfo";
 import { date } from "quasar";
 
 const userStore = useUserStore();
-const { email, metadata , uid } = userStore.getUserInfo || {};
+const { email, metadata, uid } = userStore.getUserInfo || {};
 const { createdAt } = metadata || {};
 const dateStr = date.formatDate(parseInt(createdAt), "YYYY-MM-DD");
 </script>
 
 <template>
   <q-page class="flex column mx-4">
-    <div v-if="email && metadata" class="w-full md:w-1/2 md:mx-auto my-4 p-4 infoPage">
+    <div class="w-full md:w-1/2 md:mx-auto my-4 p-4 infoPage">
       <q-avatar font-size="1.5rem" color="primary" text-color="white">{{
         email[0]
       }}</q-avatar>
