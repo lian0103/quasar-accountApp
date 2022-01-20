f61d4cc75a9c5fdda6d37e7e8d30075c9d4db260
Jason <gt07891@greattree.com.tw>
2022-01-20 17:26:13 +0800

    fix

--- src/layouts/LandingPage.vue
+++ src/layouts/LandingPage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HeaderVue from "src/components/Header.vue";
+import HeaderVue from "../components/Header.vue";
 </script>
 
 <template>
@@ -21,7 +21,7 @@ import HeaderVue from "src/components/Header.vue";
             並搭配Google Firebase平台API，實作權限註冊、儲存即時資料和專案佈署。
           </p>
           <div class="flex my-4">
-            <img class="h-12 mr-5" src="../assets/ms-icon-144x144.png" />
+            <img class="h-12 mr-5" src="https://i.imgur.com/Xrd4Tlm.png" />
             <img
               class="h-12 mr-2"
               src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
@@ -37,6 +37,7 @@ import HeaderVue from "src/components/Header.vue";
               icon="fas fa-plus-circle"
               size="md"
               color="secondary"
+              to="/signup"
               rounded
             />
           </div>
@@ -62,20 +63,23 @@ import HeaderVue from "src/components/Header.vue";
           Features
         </h2>
         <div class="flex items-center flex-wrap mb-20">
-          <div class="w-full md:w-1/2">
+          <div class="w-full md:w-1/3">
             <h4 class="text-3xl text-gray-800 font-bold mb-3">權限</h4>
             <p class="text-gray-600 mb-8 leading-loose">
               1.註冊user<br />
               2.登入/登出
             </p>
           </div>
-          <div
-            class="w-full md:w-1/2 h-72 bg-left-top bg-cover bg-[url('https://i.imgur.com/HRa84Fg.jpg')] shadow-xl"
-          ></div>
+          <div class="w-full md:w-2/3 h-auto shadow-xl">
+            <img
+              class="object-fill h-70 mx-auto"
+              src="https://i.imgur.com/HRa84Fg.jpg"
+            />
+          </div>
         </div>
 
         <div class="flex items-center flex-wrap mb-20 md:flex-row-reverse">
-          <div class="w-full md:w-1/2 md:pl-10">
+          <div class="w-full md:w-1/3 md:pl-10">
             <h4 class="text-3xl text-gray-800 font-bold mb-3">記帳</h4>
             <p class="text-gray-600 mb-8 leading-loose">
               1. 新/刪/查/改 消費紀錄<br />
@@ -83,9 +87,12 @@ import HeaderVue from "src/components/Header.vue";
               3. 消費紀錄統計圖表
             </p>
           </div>
-          <div
-            class="w-full md:w-1/2 h-72 bg-auto bg-[url('../assets/demo.png')] shadow-xl"
-          ></div>
+          <div class="w-full md:w-2/3 h-auto shadow-xl">
+            <img
+              class="object-fill h-70 mx-auto"
+              src="https://i.imgur.com/80BpjBe.png"
+            />
+          </div>
         </div>
       </section>
     </q-page-container>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HeaderVue from "src/components/Header.vue";
+import HeaderVue from "../components/Header.vue";
 import { useUserStore } from "../stores/userInfo";
 import { useRouter } from "vue-router";
 
