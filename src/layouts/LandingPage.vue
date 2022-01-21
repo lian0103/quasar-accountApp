07f603227ddc1cd2065d6d7122cc492612439ec9
Jason <gt07891@greattree.com.tw>
2022-01-21 14:48:16 +0800

    update 字體 & PWA介紹

--- src/layouts/LandingPage.vue
+++ src/layouts/LandingPage.vue
@@ -8,17 +8,13 @@ const slide = ref("1");
 <template>
   <q-layout view="lHh Lpr lFf">
     <HeaderVue />
-    <q-page-container
-      class="text-gray-700 container mx-auto"
-    >
+    <q-page-container class="text-gray-700 container mx-auto">
       <div
         class="py-12"
         style="background: linear-gradient(90deg, #667eea 0%, #764ba2 100%)"
       >
         <div class="w-full md:w-1/2 mx-auto px-6">
-          <h2 class="text-2xl mb-2 text-white font-Oswald-500">
-            Account APP
-          </h2>
+          <h2 class="text-2xl mb-2 text-white font-Oswald-500">Account APP</h2>
           <p class="text-lg my-4 text-gray-200 leading-normal">
             這次主要想測試Quasar(VueJS
             UI庫)，來實現記帳的應用，使用包含Quasar的Tab、Dialog、Form、和Dialog等組件。
@@ -49,7 +45,9 @@ const slide = ref("1");
       </div>
 
       <section class="w-full md:w-1/2 mx-auto px-6 p-10">
-        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8 font-Oswald-500">
+        <h2
+          class="text-4xl font-bold text-center text-gray-800 mb-8 font-Oswald-500"
+        >
           Stacks
         </h2>
         <p class="text-lg my-4 leading-normal">
@@ -63,12 +61,14 @@ const slide = ref("1");
       </section>
 
       <section class="w-full md:w-1/2 mx-auto px-6 p-10">
-        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8 font-Oswald-500">
+        <h2
+          class="text-4xl font-bold text-center text-gray-800 mb-8 font-Oswald-500"
+        >
           Features
         </h2>
         <div class="flex items-center flex-wrap mb-20">
           <div class="w-full md:w-1/3">
-            <h4 class="text-3xl text-gray-800 font-bold mb-3 ">權限</h4>
+            <h4 class="text-3xl text-gray-800 font-bold mb-3">權限</h4>
             <p class="text-gray-600 mb-8 leading-loose">
               1.Email/Google帳號註冊<br />
               2.登入/登出
@@ -101,10 +101,14 @@ const slide = ref("1");
       </section>
 
       <section class="w-full md:w-1/2 mx-auto px-6 p-10">
-        <h2 class="text-4xl font-bold text-center text-gray-800 mb-8 font-Oswald-500">PWA</h2>
+        <h2
+          class="text-4xl font-bold text-center text-gray-800 mb-8 font-Oswald-500"
+        >
+          PWA
+        </h2>
         <p class="text-lg my-4 leading-normal">
           透過Quasar
-          cli可以打包PWA版本，在桌機chrome和移動端sarfari中可以下載成。
+          cli可以打包PWA版本，在桌機chrome和移動端sarfari中可以下載使用。<br />
         </p>
         <div class="w-4/5 mx-auto">
           <q-carousel
