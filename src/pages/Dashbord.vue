65c26f3839d337acce702efd8ff5dc77626c86ec
Jason <gt07891@greattree.com.tw>
2022-01-26 09:03:11 +0800

    css

--- src/pages/Dashbord.vue
+++ src/pages/Dashbord.vue
@@ -4,13 +4,12 @@ const title = "PageTitle";
 
 <template>
   <div class="infoBox mx-auto flex justify-center text-white h-screen">
-    <div class=" bg-dark w-full px-4 py-6">
+    <div class="bg-dark w-full px-4 py-6">
       <div class="row text-2xl">
         <div class="col-12 col-md-8 mx-auto">
           <div
             class="relative border-8 rounded shadow-2xl font-mono text-7xl md:text-9xl grid grid-cols-2 gap-x-px"
           >
-
             <!-- left side -->
             <div class="relative bg-black p-8">
               <!-- background grid of black squares -->
@@ -21,7 +20,9 @@ const title = "PageTitle";
 
               <!-- time numbers -->
               <span class="relative left-2 md:left-4">01</span>
-              <span class="absolute right-2 bottom-2 text-xl font-Noto">月</span>
+              <span class="absolute right-2 bottom-2 text-xl font-Noto"
+                >月</span
+              >
 
               <!-- line across the middle -->
               <div class="absolute inset-0 flex items-center">
@@ -38,7 +39,9 @@ const title = "PageTitle";
               </div>
 
               <span class="relative left-4">25</span>
-              <span class="absolute right-2 bottom-2 text-xl font-Noto">日</span>
+              <span class="absolute right-2 bottom-2 text-xl font-Noto"
+                >日</span
+              >
 
               <!-- line across the middle -->
               <div class="absolute inset-0 flex items-center">
@@ -49,12 +52,9 @@ const title = "PageTitle";
         </div>
       </div>
       <div class="row">
-               <div class="h-px w-full md:w-3/4 mx-auto border-2 mt-6 md:mt-12"></div>
+        <div class="h-px w-full md:w-3/4 mx-auto border-2 mt-6 md:mt-12"></div>
       </div>
     </div>
-
-
-
   </div>
 </template>
 
