510775cf8b27a366924017168cdb23ee53c46e22
Jason <gt07891@greattree.com.tw>
2022-01-28 09:44:59 +0800

    firebase 部屬設定

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyBncSexDmqEHo1V_SgpHsi6FUdiNiiayoA",
-  authDomain: "account-app-f70c0.firebaseapp.com",
-  projectId: "account-app-f70c0",
-  storageBucket: "account-app-f70c0.appspot.com",
-  databaseURL:
-    "https://account-app-f70c0-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  messagingSenderId: "893886700460",
-  appId: "1:893886700460:web:fbcc5bd3fc95013714f809",
-  measurementId: "G-J6SMYP2MZM",
+  apiKey: "AIzaSyCp9kcAHaK5v53QRzO3FGw9liOT5l9NvMw",
+  authDomain: "gt-record.firebaseapp.com",
+  projectId: "gt-record",
+  storageBucket: "gt-record.appspot.com",
+  messagingSenderId: "230029838774",
+  appId: "1:230029838774:web:5c5d430ca620632bfe5917"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
