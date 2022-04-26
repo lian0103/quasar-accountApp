5f500c0a21e66f99f40c65a7cc6a25cdf41cb313
Jason <gt07891@greattree.com.tw>
2022-04-26 17:03:02 +0800

    config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,18 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-const firebaseConfig = {
-  apiKey: "AIzaSyBncSexDmqEHo1V_SgpHsi6FUdiNiiayoA",
-  authDomain: "account-app-f70c0.firebaseapp.com",
-  projectId: "account-app-f70c0",
-  storageBucket: "account-app-f70c0.appspot.com",
-  databaseURL:
-    "https://account-app-f70c0-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  messagingSenderId: "893886700460",
-  appId: "1:893886700460:web:fbcc5bd3fc95013714f809",
-  measurementId: "G-J6SMYP2MZM",
-};
+const firebaseConfig = require('../../firebaseConfig').default;
 
+// console.log(firebaseConfig)
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
